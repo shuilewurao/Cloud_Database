0e54b94c44ffcd92aa296fd8ee0eefdff6fed90e
Frida Tian <frida.tian@mail.utoronto.ca>
2020-02-17 19:19:19 -0500

    complete M1

---
+++ ece419_M1/src/app_kvServer/CacheManager/CachePolicy.java
@@ -0,0 +1,58 @@
+package app_kvServer.CacheManager;
+
+import java.util.Map;
+
+public abstract class CachePolicy {
+    protected int maxCacheSize;
+
+    protected Map<String, String> cacheList;
+
+    public CachePolicy(int cachesize) {
+        this.maxCacheSize = cachesize;
+    }
+
+    public boolean inCache(String key) {
+        synchronized (cacheList) {
+            return cacheList.containsKey(key);
+        }
+    }
+
+    protected void deleteCache(String key) {
+        synchronized (cacheList) {
+            cacheList.remove(key);
+        }
+    }
+
+    public String getKV(String key) {
+        synchronized (cacheList) {
+            if (cacheList.containsKey(key)) {
+                return cacheList.get(key);
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public void putKV(String key, String value) {
+        synchronized (cacheList) {
+            //TODO: invalid value
+
+            if (value == null) {
+                if (inCache(key)) {
+                    deleteCache(key);
+                }
+            } else {
+                cacheList.put(key, value);
+            }
+        }
+    }
+
+    public void clearCache() {
+        cacheList.clear();
+    }
+
+    protected int getCacheSize() {
+        return this.maxCacheSize;
+    }
+
+}
---
+++ ece419_M1/src/app_kvServer/CacheManager/FIFO.java
@@ -0,0 +1,22 @@
+package app_kvServer.CacheManager;
+
+import java.util.*;
+
+
+public class FIFO extends CachePolicy {
+
+    public FIFO(int cacheSize) {
+        super(cacheSize);
+        this.cacheList = Collections.synchronizedMap(
+                new LinkedHashMap<String, String>(
+                        cacheSize
+                ){
+                    @Override
+                    protected boolean removeEldestEntry(Map.Entry eldest) {
+                        return size() > getCacheSize();
+                    }
+                }
+        );
+    }
+
+}
