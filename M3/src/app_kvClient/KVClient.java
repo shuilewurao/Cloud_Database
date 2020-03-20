5acdce83a8f318fd3e17778e1f9056ecfa5f198b
liuzha11 <59938271+liuzha11@users.noreply.github.com>
2020-03-20 10:33:42 -0700

    failure handling on client side is done

--- M3/src/app_kvClient/KVClient.java
+++ M3/src/app_kvClient/KVClient.java
@@ -20,10 +20,13 @@ public class KVClient implements IKVClient {
     private static final String PROMPTIN = "KVClient << ";
     private KVStore client = null;
     private boolean stop = false;
-
+    public boolean serverDown;
+    public String latestRequest;
+    public String latestAddress;
     private String serverAddress;
+    public int latestPort;
     private int serverPort;
-
+    public boolean initialConnectionError;
     public void run() {
         while (!stop) {
             BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
@@ -40,10 +43,17 @@ public class KVClient implements IKVClient {
     }
 
     private void handleCommand(String cmdLine) {
-        String cmdLineTrim = cmdLine.trim();
+        serverDown = false;
+        initialConnectionError = false;
+        String cmdLineTrim;
+        if(cmdLine.equals("re-handle"))
+            cmdLineTrim = latestRequest;
+        else
+            cmdLineTrim = cmdLine.trim();
+        latestRequest = cmdLineTrim;
         logger.debug("[KVClient] Trimmed: " + cmdLineTrim);
         String[] tokens = cmdLineTrim.split("\\s+");
-
+        
         switch (tokens[0]) {
             case "quit":
                 stop = true;
@@ -59,6 +69,8 @@ public class KVClient implements IKVClient {
                     try {
                         serverAddress = tokens[1];
                         serverPort = Integer.parseInt(tokens[2]);
+                        latestAddress = serverAddress;
+                        latestPort = serverPort;
                         newConnection(serverAddress, serverPort);
                     } catch (NumberFormatException nfe) {
                         printError("No valid address. Port must be a number!");
@@ -67,6 +79,8 @@ public class KVClient implements IKVClient {
                         printError("Unknown Host!");
                         logger.info("[KVClient] Unknown Host!", e);
                     } catch (Exception e) {
+                        serverDown = true;
+                        initialConnectionError = true;
                         printError("Could not establish connection!");
                         logger.warn("[KVClient] Could not establish connection!", e);
                     }
@@ -74,7 +88,6 @@ public class KVClient implements IKVClient {
                     logger.error("[KVClient] Invalid number of parameters!");
                     printError("Invalid number of parameters!");
                 }
-
                 break;
             case "put":
                 if (tokens.length >= 2) {
@@ -99,12 +112,15 @@ public class KVClient implements IKVClient {
                                     logger.info("[KVClient] PUT_UPDATE" + " for key = " + tokens[1] + ", " + "value = " + tokens[2]);
                                     System.out.println(PROMPT + "PUT_UPDATE" + " for key = " + tokens[1] + ", " + "value = " + tokens[2]);
                                 } else if (status == StatusType.SERVER_NOT_RESPONSIBLE) {
+                                    serverDown = true;
                                     logger.info("[KVClient] SERVER_NOT_RESPONSIBLE" + " for key = " + tokens[1] + ", " + "value = " + tokens[2]);
                                     System.out.println(PROMPT + "SERVER_NOT_RESPONSIBLE" + " for key = " + tokens[1] + ", " + "value = " + tokens[2]);
                                 } else if (status == StatusType.SERVER_STOPPED) {
+                                    serverDown = true;
                                     logger.info("[KVClient] SERVER_STOPPED");
                                     System.out.println(PROMPT + "SERVER_STOPPED");
                                 } else if (status == StatusType.SERVER_WRITE_LOCK) {
+                                    //serverDown = true;
                                     logger.info("[KVClient] SERVER_WRITE_LOCK");
                                     System.out.println(PROMPT + "SERVER_WRITE_LOCK");
                                 } else {
@@ -112,7 +128,8 @@ public class KVClient implements IKVClient {
                                 }
 
                             } catch (Exception e) {
-                                logger.error("[KVClient] PUT_ERROR: " + e);
+                                serverDown = true;
+                                logger.error("[KVClient] damn it is this real?? PUT_ERROR: " + e);
                                 e.printStackTrace();
 
                             }
@@ -130,15 +147,18 @@ public class KVClient implements IKVClient {
                                     logger.error("[KVClient] DELETE_ERROR");
                                     System.out.println(PROMPT + "DELETE_ERROR");
                                 } else if (status == StatusType.SERVER_NOT_RESPONSIBLE) {
+                                    serverDown = true;
                                     logger.info("[KVClient] SERVER_NOT_RESPONSIBLE" + " for key = " + tokens[1]);
                                     System.out.println(PROMPT + "SERVER_NOT_RESPONSIBLE" + " for key = " + tokens[1]);
                                 } else if (status == StatusType.SERVER_STOPPED) {
+                                    serverDown = true;
                                     logger.info("[KVClient] SERVER_STOPPED");
                                     System.out.println(PROMPT + "SERVER_STOPPED");
                                 } else {
                                     logger.error("[KVClient] UNKNOWN DELETE ERROR");
                                 }
                             } catch (Exception e) {
+                                serverDown = true;
                                 logger.error("[KVClient] ERROR FOR PUT: " + e);
                                 System.out.println(PROMPT + "ERROR FOR PUT: " + e);
                                 e.printStackTrace();
@@ -175,15 +195,18 @@ public class KVClient implements IKVClient {
                                 logger.error("[KVClient] GET_ERROR");
                                 System.out.println(PROMPT + "GET_ERROR");
                             } else if (status == StatusType.SERVER_NOT_RESPONSIBLE) {
+                                serverDown = true;
                                 logger.info("[KVClient] SERVER_NOT_RESPONSIBLE" + " for key = " + key + ", " + "value = " + value);
                                 System.out.println(PROMPT + "SERVER_NOT_RESPONSIBLE" + " for key = " + key + ", " + "value = " + value);
                             } else if (status == StatusType.SERVER_STOPPED) {
+                                serverDown = true;
                                 logger.info("[KVClient] SERVER_STOPPED");
                                 System.out.println(PROMPT + "SERVER_STOPPED");
                             } else {
                                 logger.error("[KVClient] UNKNOWN DELETE ERROR");
                             }
                         } catch (Exception e) {
+                            serverDown = true;
                             logger.error("[KVClient] GET_ERROR + exception");
                             logger.error(e.getStackTrace());
                         }
@@ -225,6 +248,42 @@ public class KVClient implements IKVClient {
                 printError("Unknown command");
                 printHelp();
                 break;
+        }
+        if(serverDown){
+            int handleResult = handleServerDown();
+            if(handleResult == 0)
+                printError("All servers are currently unavailable!");
+            else
+                System.out.println("Connection resumed and previous client request executed");
+        }   
+    }
+    
+    public int handleServerDown(){
+        //disconnect();
+        serverDown = true;
+        int serverNode = -1;
+        int attemp = 0;
+        while(serverDown == true){
+            try {
+                serverNode++;
+                attemp++;
+                if(attemp > 16)
+                    return 0;
+                latestAddress = "localhost";
+                latestPort = 50000 + (serverNode % 8);
+                newConnection(latestAddress, latestPort);
+                serverDown = false;
+            }catch (Exception e) {
+                serverDown = true;
+                printError("Could not establish connection!");
+                logger.warn("[KVClient] Could not establish connection!", e);
+            }
+        }
+        if(initialConnectionError == true)
+            return 1;
+        else{
+            this.handleCommand("re-handle");
+            return 1;
         }
     }
 
